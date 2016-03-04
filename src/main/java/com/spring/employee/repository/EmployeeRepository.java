f4d7a44b8cd0ff2b26a5fa9eaccf2574998722e6
Kiran <Kiran_Ramreddy@gap.com>
2016-03-04 22:48:37 +0530

    "added spring jdbc configuration using xml and application.properties file for employee flow. added ojdbc14.jar at lib folder. removed applicationContext.xml as it is not necessary. added db.xml, spring-beans.xml for bean instantiation"

--- src/main/java/com/spring/employee/repository/EmployeeRepository.java
+++ src/main/java/com/spring/employee/repository/EmployeeRepository.java
@@ -1,8 +1,20 @@
 package com.spring.employee.repository;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.spring.domain.employee.Employee;
 
 @Repository
 public class EmployeeRepository {
+	
+	private JdbcTemplate springJdbcTemplate;
+
+	//TODO:Need to implement merge query if the employee record already present
+	// update it; else insert it
+	public void saveEmployee(Employee employee) {
+		String query = "";
+		springJdbcTemplate.execute(query);
+	}
 
 }
--- src/main/java/com/spring/employee/service/EmployeeService.java
+++ src/main/java/com/spring/employee/service/EmployeeService.java
@@ -17,21 +17,25 @@ public class EmployeeService {
 
 	public void saveEmployee(Employee employee) {
 		validateEmployee(employee);
+		empoloyeeRepository.saveEmployee(employee);
 		
 	}
 
 	private void validateEmployee(Employee employee) {
-		if(StringUtils.isBlank(employee.getEmpId()) || StringUtils.isBlank(employee.getEmpName())){
-			validateEmployeeSalaryAndAge(employee);
+		if(StringUtils.isBlank(employee.getEmpId()) || StringUtils.isBlank(employee.getEmpName()) || validateEmployeeSalaryAndAge(employee)){
+			throw new EmployeevalidationFailedException("employee details not valid : all fields are not nullable. emp salary should be >1000 ; age should be >18");
 		}
 		
 	}
 
-	private void validateEmployeeSalaryAndAge(Employee employee) {
-		if(Optional.ofNullable(employee.getEmpAge()).isPresent() || Optional.ofNullable(employee.getEmpSalary()).isPresent() || employee.getEmpAge()<=18 || employee.getEmpSalary()<=1000) {
-			throw new EmployeevalidationFailedException("employee details not valid : all fields are not nullable. emp salary should be >1000 ; age should be >18");
-			
+	private boolean validateEmployeeSalaryAndAge(Employee employee) {
+		boolean a = false;
+		if(!Optional.ofNullable(employee.getEmpAge()).isPresent()
+				|| !Optional.ofNullable(employee.getEmpSalary()).isPresent() || employee.getEmpAge() <= 18
+				|| employee.getEmpSalary() <= 1000) {
+			a = true;
 		}
+		return a;
 	}
 	
 
